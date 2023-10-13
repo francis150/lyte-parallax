9a19ac4ea0bafe9432ab52c5f089a3711e1983d5
Francis Dela Victoria <francisdelavictoria150@gmail.com>
2023-10-13 14:40:54 +0800

    initial commit

---
+++ lyte-parallax.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * 
+ * @package RankAssassin
+ * 
+ * Plugin Name: Lyte Parallax
+ * Description: Dont know yet
+ * Version: 1.0.0
+ * Requires at least: 5.2
+ * Tested up to: 5.9
+ * Requires PHP: 7.2
+ * Author: _lyte
+ * Author URI: https://lyte-wp.com/
+ * Text Domain: rank-assassin
+ */
+
+defined( 'ABSPATH' ) or die( 'Looks like someone just took a headshot!' );
+
+// Require the core php file with class
+// require plugin_dir_path( __FILE__ ) . 'includes/LyteParallaxCore.php';
+// new LyteParallaxCore();
